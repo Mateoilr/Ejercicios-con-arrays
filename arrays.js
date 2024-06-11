c1272673638f0ca5012501bd39196bc6ebb50230
Mateo Lituma <milituma@sudamericano.edu.ec>
2024-06-10 20:28:43 -0500

    tarea

---
+++ arrays.js
@@ -0,0 +1,28 @@
+'use strict';
+
+//Escribir una función que reciba un array de strings y muestre cada elemento con su posición.
+//Usar únicamente for o while para los bucles.
+let elementos = ['carro', 'perro', 'azucar', 'lampara', 'celular'];
+for (let i = 0; i < palabras.length; i++) {
+    alert('En la posición ' + i + ' se encuentra el elemento ' + palabras[i]);
+}
+
+//Escribir una función que reciba un array de números y devuelva la suma de todos sus elementos.
+//Usar únicamente for o while para los bucles.
+let suma = 0;
+let numbers = [121, 22, 43, 94, 25];
+for (let i = 0; i < 5; i++) {
+    suma = suma + numeros[i];
+    alert('la suma es:'+suma);
+} 
+
+//Escribir una función que reciba un array de números y muestre casa uno de sus elementos multiplicado por 3.
+//Usar únicamente for o while para los bucles.
+let numero = [14, 52, 3, 56, 15];
+for (let i = 0; i < 5; i++) {
+    alert('El elemento ' + i + ' multiplicado por 3 es ' + number[i] * 3);
+}
+
+
+
+
